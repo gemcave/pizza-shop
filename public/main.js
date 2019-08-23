e9c9dd2ad2ea8e75a6c6ee3312bd5a50a2f8b4ac
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-08-23 22:39:14 +0600

    update add_to_cart function

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
-function add_to_cart() {
-	alert('Pizza added.')
-}
+function add_to_cart(id) {
+	
+	let key = `product_${id}`;
+	let x = window.localStorage.getItem(key);
+
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+} 
