507448ac3df9b4f6137bd73854e90fbbfac224d5
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-08-24 20:58:28 +0600

    update orders button

--- public/main.js
+++ public/main.js
@@ -8,6 +8,7 @@ function add_to_cart(id) {
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 } 
 
 function cart_get_number_of_items() {
@@ -26,6 +27,10 @@ function cart_get_number_of_items() {
 function update_orders_input() {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+function update_orders_button() {
+	var text = `Cart(${cart_get_number_of_items()})`;
+	$('#orders_button').val(text);
 }
 
 function cart_get_orders() {
